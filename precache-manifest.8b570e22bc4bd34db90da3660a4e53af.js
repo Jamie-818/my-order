6963252e6de9036b0e7fb093527be86aaf1e7452
Joeyscat <zhouyu.fun@gmail.com>
2019-05-12 15:06:13 +0800

    Updates

---
+++ precache-manifest.8b570e22bc4bd34db90da3660a4e53af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a50c24f42159c8e4d97f171ec52ef3",
+    "url": "/my-order/index.html"
+  },
+  {
+    "revision": "a66d43d3e058d717cb70",
+    "url": "/my-order/static/css/main.e934d604.chunk.css"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/my-order/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "a66d43d3e058d717cb70",
+    "url": "/my-order/static/js/main.62422576.chunk.js"
+  },
+  {
+    "revision": "727410009b2946ab5653",
+    "url": "/my-order/static/js/runtime~main.c9d33ab8.js"
+  }
+]);
